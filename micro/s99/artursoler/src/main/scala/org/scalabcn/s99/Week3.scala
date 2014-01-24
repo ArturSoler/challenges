ae23cd56223f3df475b579dba4615f97c4c05a1b
Artur Soler <artur.soler@gmail.com>
2014-01-24 13:59:37 +0100

    Add solution 22.

--- micro/s99/artursoler/src/main/scala/org/scalabcn/s99/Week3.scala
+++ micro/s99/artursoler/src/main/scala/org/scalabcn/s99/Week3.scala
@@ -5,4 +5,12 @@ object Week3 {
     case 0 => elem :: xs
     case _ => xs.head :: insertAt(elem, pos-1, xs.tail)
   }
+
+  def range(left: Int, right: Int) = {
+    def inner(count: Int, accum: List[Int]): List[Int] = if(count == left) count :: accum else inner(count-1, count :: accum)
+    if (left <= right)
+      inner(right, Nil)
+    else
+      Nil
+  }
 }
--- micro/s99/artursoler/src/test/scala/org/scalabcn/s99/Week3FlatSpec.scala
+++ micro/s99/artursoler/src/test/scala/org/scalabcn/s99/Week3FlatSpec.scala
@@ -5,13 +5,23 @@ import org.scalatest.prop.PropertyChecks
 import org.scalabcn.s99.Week3._
 
 class Week3FlatSpec  extends FlatSpec with Matchers with PropertyChecks {
-  "insertAt (P20)" should "insert an element at a given position into a list" in {
+  "insertAt (P21)" should "insert an element at a given position into a list" in {
     insertAt('new, 1, List('a, 'b, 'c, 'd)) should be (List('a, 'new, 'b, 'c, 'd))
 
     forAll { (left: List[Int], elem: Int, right: List[Int]) =>
       val joined = left ::: elem :: right
       val before = left ::: right
       insertAt(elem, left.length, before) should be (joined)
+    }
+  }
+
+  "range (P22)" should "create a list containing all integers within a given range" in {
+    range(4, 9) should be (List(4, 5, 6, 7, 8, 9))
+
+    forAll { (left: Int, right: Int) =>
+      val min = left min right
+      val max = (left max right) min (min + 10000)
+      range(min, max) should be ((min to max).toList)
     }
   }
 }
